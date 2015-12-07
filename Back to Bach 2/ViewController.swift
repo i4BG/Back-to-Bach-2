dd071f18d9e0a3a9ad76876f24fa4244d9d4afcc
Ivaylo Todorov <ivaylo.k.todorov@me.com>
2015-12-07 21:48:28 +0200

    Back to Back
    Simple Music player with Play, Pause, Stop, Volume and scub

--- Back to Bach 2/ViewController.swift
+++ Back to Bach 2/ViewController.swift
@@ -7,12 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer = AVAudioPlayer()
+    
+    @IBAction func play(sender: AnyObject) {
+        player.play()
+    }
+    @IBAction func pause(sender: AnyObject) {
+        player.pause()
+    }
+    @IBAction func stop(sender: AnyObject) {
+        
+        player.pause()
+        
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!))
+           
+        } catch {
+        }
+    }
+    
+    func updateScrubSlider() {
+        
+        scrubSlider.value = Float(player.currentTime)
+        
+    }
+    
+    @IBAction func adjustVolume(sender: AnyObject) {
+        player.volume = volumeSlider.value
+    }
+    @IBOutlet var volumeSlider: UISlider!
+    @IBOutlet var scrubSlider: UISlider!
+    @IBAction func scrub(sender: AnyObject) {
+        
+        player.currentTime = NSTimeInterval(scrubSlider.value)
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        do {
+            
+            try player = AVAudioPlayer(contentsOfURL: NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("bach", ofType: "mp3")!))
+            scrubSlider.maximumValue = Float(player.duration)
+            
+        } catch {
+            
+    }
+        
+        _ = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("updateScrubSlider"), userInfo: nil, repeats: true)
+        
     }
 
     override func didReceiveMemoryWarning() {
